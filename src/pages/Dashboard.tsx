27d983a1517667f5f4786aa2daee0f0ba39c9540
DedaloApps <nunoguimaraes@dedalo.pt>
2025-06-30 10:39:12 +0100

    Primeiro commit do projeto Backoffice Sintra360

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { FaUsers, FaExclamationTriangle, FaLightbulb, FaCalendarAlt } from 'react-icons/fa';
 import { db } from '../firebase';
